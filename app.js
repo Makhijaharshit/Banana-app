2c7815be5e24da456194da35fd3054e95d39b7b1
Makhijaharshit <harsh.makhija1809@gmail.com>
2021-12-05 21:13:32 +0530

    banana-app

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input") 
 var outputDiv=  document.querySelector("#output")
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 function getTranslationnURL(text){
     return serverURL + "?"+ "text"+ text
 }
 function errorHandler(error){
-    console.log("error occured", error)
+    console.log("error occured", error);
     alert("something wrong with server try again next time")
 }
 
@@ -17,7 +17,12 @@ function clickHandler(){
 
     fetch(getTranslationnURL(inputText))
     .then(response=>response.json())
-    .then(json => console.log(json.contents.translated))
+    .then(json => { 
+        var translatedText = json.contents.translated
+        outputDiv.innerText = translatedText
+
+    })
     .catch(errorHandler)
 };
+
 btnTranslate.addEventListener("click",clickHandler)
