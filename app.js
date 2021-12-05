328753a9ca2d0f7f440652ff25e0542fe4c2115f
Makhijaharshit <harsh.makhija1809@gmail.com>
2021-12-05 20:51:12 +0530

    error handler done

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 function getTranslationnURL(text){
     return serverURL + "?"+ "text"+ text
 }
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("something wrong with server try again next time")
+}
 
 function clickHandler(){
     // outputDiv.innerText="botttommmm " + txtInput.value;
@@ -14,5 +18,6 @@ function clickHandler(){
     fetch(getTranslationnURL(inputText))
     .then(response=>response.json())
     .then(json => console.log(json.contents.translated))
+    .catch(errorHandler)
 };
 btnTranslate.addEventListener("click",clickHandler)
